19a10b2c33cc1b95dcddef884b3de6e494b09cd9
TANAMOI <34171842+TANAMOI@users.noreply.github.com>
2018-01-19 17:52:43 +0000

    Update Ejercicio11.java

--- src/com/cip/prog/Ejercicio11.java
+++ src/com/cip/prog/Ejercicio11.java
@@ -7,7 +7,7 @@ public class Ejercicio11 {
 		int[] numeros= {1,524,423,825,1524,324,899,975,775,657};
 		//numeros = new int[10];
 		
-		for(int n =0; n< numeros.length;n++) {
+		for(int n =0; n< numeros;n++) {
 			System.out.println(numeros[n]);
 			
 		}
